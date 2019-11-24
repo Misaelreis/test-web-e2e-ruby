4a6858bfc3f4b02e1a7d910108758aa73bd4cfc7
Misaelreis <zael.au@gmail.com>
2019-11-24 17:30:14 -0300

    Update

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,7 +10,7 @@ World(Helper)
 World(Pages)
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_headless
     config.app_host = CONFIG['url_padrao']
     config.default_max_wait_time = 10
 end
